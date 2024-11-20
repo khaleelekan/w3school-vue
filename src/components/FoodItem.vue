eca47ec9d400d2779159ad109ec44ec5a379ccbb
khalilullahi abdulraheem <kalilulekan@gmail.com>
2024-11-20 13:48:49 +0100

    starter vue with progress

---
+++ src/components/FoodItem.vue
@@ -0,0 +1,33 @@
+<template>
+    <div @:click="increment">
+        <h2>{{ name }}</h2>
+        <p>{{ message }}</p>
+        <div id="red">{{ count }}</div>
+    </div>
+    
+</template>
+
+<script>
+
+
+export default{
+    data(){
+        return{
+            name: 'Apples',
+            message: 'I like apples',
+            count: 0
+        }
+    },
+    methods:{
+        increment(){
+            this.count++;
+        }
+    }
+}
+</script>
+
+<style>
+ #red {
+    font-weight: bold ;
+    color: rgb(144, 12, 12);
+  }</style>
