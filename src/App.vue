c5b64312a5f3c340249764ed7e6f77bb4c493411
khalilullahi abdulraheem <kalilulekan@gmail.com>
2024-11-20 23:13:24 +0100

    props validator

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,16 @@
   <h1>{{ message }}</h1>
   <food-item 
   food-name="Apples"
-  food-desc="apples can come in red and green"/>
+  food-desc="apples can come in red and green"
+  v-bind:is-favorite="true"/>
   <food-item
    food-name="Pizza" 
-   food-desc="apples can come in red and green"/>
+   food-desc="apples can come in red and green"
+   v-bind:is-favorite="false"/>
   <food-item 
   food-name="Rice" 
-  food-desc="apples can come in red and green"/>
+  food-desc="apples can come in red and green"
+  v-bind:is-favorite="false"/>
 </template>
 
 <script >
--- src/components/FoodItem.vue
+++ src/components/FoodItem.vue
@@ -2,17 +2,41 @@
     <div @:click="increment">
         <h2>{{ foodName }}</h2>
         <h2>{{ foodDesc }}</h2>
+        <img v-show="isFavorite" src="https://images.unsplash.com/photo-1511690656952-34342bb7c2f2?q=80&w=928&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="">
     </div>
     
 </template>
 
 <script>
+import { type } from 'os';
+
 
 
 export default{
-    props:[
-        'foodName','foodDesc'
-    ],
+    props:{
+        foodName:{
+            type: String,
+            required: true,
+        },
+        foodDesc:{
+            type: String,
+            required: false,
+            default: 'this is a default fooddesc',
+            validator: function(){
+                if (20 < value.length && value.length < 50){
+                    return true
+                }else{
+                    return false
+                }
+            }
+        },
+        isFavorite:{
+            type: Boolean,
+            required: false,
+            default: false
+
+        }
+    },
     data(){
         return{
             name: 'Apples',
@@ -32,4 +56,9 @@ export default{
  #red {
     font-weight: bold ;
     color: rgb(144, 12, 12);
-  }</style>
+  }
+  img{
+    width: 80%;
+    height: 80%;
+  }
+  </style>
